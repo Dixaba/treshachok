7ac34730948ece6aad11c25ff31e3a3ee773b387
Dixaba <dixaba@yandex.ru>
2019-04-08 13:40:53 +0300

    Без нейронки-самописьки

--- mainwindow.h
+++ mainwindow.h
@@ -34,10 +34,6 @@ class MainWindow : public QMainWindow {
     void on_pushButton_8_clicked();
     void on_pushButton_9_clicked();
 
-    void on_pushButton_10_clicked();
-
-    void on_pushButton_11_clicked();
-
   private:
     Ui::MainWindow *ui;
 
